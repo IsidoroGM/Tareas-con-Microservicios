b4b9dbd36d913a04e5f94b16f35f5c5eaffd15ca
Iisidoro Gordillo <igm27292@gmail.com>
2026-08-27 17:11:43 +0200

    Añadir soporte para despliegue en servidor Tomcat y configuración de la clase principal de la aplicación

--- src/main/java/MicroS/app/AppMicroServiciosApplication.java
+++ src/main/java/MicroS/app/AppMicroServiciosApplication.java
@@ -2,9 +2,17 @@ package MicroS.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AppMicroServiciosApplication {
+public class AppMicroServiciosApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(AppMicroServiciosApplication.class);
+	}
+
 
 	public static void main(String[] args) {
 		SpringApplication.run(AppMicroServiciosApplication.class, args);
